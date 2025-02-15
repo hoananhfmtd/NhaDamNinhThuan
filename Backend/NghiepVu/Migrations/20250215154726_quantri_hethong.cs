f41be00b86c87f49a476167e68084b5fe8ec92cc
hoangvt101 <vthoangtnt@gmail.com>
2025-02-15 23:02:31 +0700

    commint model quản trị hệ thống

---
+++ Backend/NghiepVu/Migrations/20250215154726_quantri_hethong.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace NghiepVu.Migrations
+{
+    /// <inheritdoc />
+    public partial class quantri_hethong : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "QTHT_Apps",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Code = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Url = table.Column<string>(type: "text", nullable: true),
+                    Note = table.Column<string>(type: "text", nullable: true),
+                    AllowedCorsOrigins = table.Column<string>(type: "text", nullable: true),
+                    ApiResources = table.Column<string>(type: "text", nullable: true),
+                    PostLogoutRedirectUris = table.Column<string>(type: "text", nullable: true),
+                    RedirectUris = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QTHT_Apps", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QTHT_DichVus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Ma = table.Column<string>(type: "text", nullable: true),
+                    Ten = table.Column<string>(type: "text", nullable: true),
+                    MoTa = table.Column<string>(type: "text", nullable: true),
+                    NgayTao = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QTHT_DichVus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QTHT_DM_DVHCs",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    TenDVHC = table.Column<string>(type: "text", nullable: true),
+                    MaTinh = table.Column<string>(type: "text", nullable: true),
+                    MaHuyen = table.Column<string>(type: "text", nullable: true),
+                    MaXa = table.Column<string>(type: "text", nullable: true),
+                    HoatDong = table.Column<int>(type: "integer", nullable: false),
+                    Cap = table.Column<int>(type: "integer", nullable: false),
+                    Ten = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QTHT_DM_DVHCs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QTHT_Functions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ModuleId = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    AppId = table.Column<int>(type: "integer", nullable: false),
+                    MaChucNang = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QTHT_Functions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QTHT_Logs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Controller_Action = table.Column<string>(type: "text", nullable: true),
+                    UserCreate = table.Column<string>(type: "text", nullable: true),
+                    Content = table.Column<string>(type: "text", nullable: true),
+                    NgayTao = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QTHT_Logs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QTHT_Modules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    AppId = table.Column<int>(type: "integer", nullable: false),
+                    ModuleCode = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QTHT_Modules", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QTHT_Roles_Functions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FunctionId = table.Column<int>(type: "integer", nullable: false),
+                    RoleId = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QTHT_Roles_Functions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QTHT_Apps");
+
+            migrationBuilder.DropTable(
+                name: "QTHT_DichVus");
+
+            migrationBuilder.DropTable(
+                name: "QTHT_DM_DVHCs");
+
+            migrationBuilder.DropTable(
+                name: "QTHT_Functions");
+
+            migrationBuilder.DropTable(
+                name: "QTHT_Logs");
+
+            migrationBuilder.DropTable(
+                name: "QTHT_Modules");
+
+            migrationBuilder.DropTable(
+                name: "QTHT_Roles_Functions");
+        }
+    }
+}
--- Backend/NghiepVu/Models/NghiepVu_Context.cs
+++ Backend/NghiepVu/Models/NghiepVu_Context.cs
@@ -9,7 +9,19 @@ public class NghiepVuContext(DbContextOptions<NghiepVuContext> options) : Identi
     public DbSet<EndUserBug> EndUserBugs { get; set; }
     public DbSet<UserEvent> UserEvents { get; set; }
     #endregion
+    #region dbset for qtht
+    public DbSet<QTHT_Apps> QTHT_Apps { get; set; }
+    public DbSet<QTHT_Modules> QTHT_Modules { get; set; }
+    public DbSet<QTHT_Functions> QTHT_Functions { get; set; }
+    public DbSet<QTHT_Roles_Functions> QTHT_Roles_Functions { get; set; }
 
+    public DbSet<QTHT_Logs> QTHT_Logs { get; set; }
+
+    public DbSet<QTHT_DM_DVHCs> QTHT_DM_DVHCs { get; set; }
+    public DbSet<QTHT_DichVus> QTHT_DichVus { get; set; }
+
+
+    #endregion
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);// builder.HasPostgresExtension("uuid-ossp");
