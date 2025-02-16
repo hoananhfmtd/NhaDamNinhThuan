8a7b30322d81c4e16c845c10d782f96276c93a73
hoananhfmtd <hoanganhfmtd@gmail.com>
2025-02-16 10:48:39 +0700

    change branch

--- Backend/NghiepVu/obj/Debug/net8.0/NghiepVu.AssemblyInfo.cs
+++ Backend/NghiepVu/obj/Debug/net8.0/NghiepVu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NghiepVu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a21e48db48d8fee9e672e4aefc6ea275bf92f3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("NghiepVu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NghiepVu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
